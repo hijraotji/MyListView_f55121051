8ce3bbd6bbe12b8d7b593bf76d0af16db0001dbf
hijraotji <hijraotji@gmail.com>
2022-10-29 08:03:13 +0800

    Initial commit

---
+++ app/src/main/java/com/hijraotji/mylistview_f55121051/Hijra.java
@@ -0,0 +1,14 @@
+package com.hijraotji.mylistview_f55121051;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hijra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hijra);
+    }
+}
